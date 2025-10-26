8fb2e83947dfa3a07cd0354af9f4f77a254c9efc
jeongjiwon <wldnjs9705@gmail.com>
2025-10-26 13:30:38 +0900

    feat: pc용 sidebar 추가

---
+++ components/layout/pc-sidebar.tsx
@@ -0,0 +1,36 @@
+'use client';
+import useSidebar from '@/hooks/useSidebar';
+import { PanelLeft } from 'lucide-react';
+import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
+export default function PcSidebar() {
+  const { isOpen, toggleSidebar } = useSidebar();
+  return (
+    <aside
+      className={cn(
+        'hidden w-64 flex-col border-r bg-gray-50 p-4 md:flex dark:bg-gray-900 transition-all duration-300',
+        isOpen || 'w-19'
+      )}
+    >
+      <div className="flex grow relative max-h-10 ">
+        <Button
+          size="icon-lg"
+          variant="ghost"
+          className="hover:bg-gray-200 p-2 z-10"
+          onClick={toggleSidebar}
+        >
+          <PanelLeft />
+        </Button>
+        <h1
+          className={cn(
+            'absolute top-1/2 -translate-y-1/2 right-0 whitespace-nowrap transition-opacity opacity-0',
+            isOpen && 'opacity-100 duration-300 delay-100'
+          )}
+        >
+          LostArk Auction
+        </h1>
+      </div>
+      <div className="grow"></div>
+    </aside>
+  );
+}
