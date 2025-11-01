3c425ec695f75cd187f333153ab33e8e75232482
jeongjiwon <wldnjs9705@gmail.com>
2025-11-01 11:51:48 +0900

    feat: dark모드를 적용하기 위해 toggle이 변경될 시 dark-mode를 html에 삽입

--- hooks/useToggle.tsx
+++ hooks/useToggle.tsx
@@ -1,5 +1,6 @@
 import { useAtom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
+import { useEffect } from 'react';
 
 const themeAtom = atomWithStorage('theme', 'light');
 export default function useToggle() {
@@ -8,6 +9,9 @@ export default function useToggle() {
   const toggleTheme = () => {
     setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
   };
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', theme === 'dark');
+  }, [theme]);
 
   return { theme, toggleTheme };
 }
