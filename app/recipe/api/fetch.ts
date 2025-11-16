b3de201b36c9ac7d4aae6cf90b487962ae107471
jeongjiwon <wldnjs9705@gmail.com>
2025-11-16 12:53:32 +0900

    feat: 각인서 정보를 가지고 오기 위한 fetch 함수 추가

---
+++ app/recipe/api/fetch.ts
@@ -0,0 +1,16 @@
+import { apiClient } from '@/lib/apiClient';
+import { RecipeRequest } from './interface';
+
+export default function getRecipeList(request: RecipeRequest) {
+  return apiClient('/markets/items', {
+    method: 'POST',
+    body: JSON.stringify({
+      ...request,
+      Sort: 'GRADE',
+      CharacterClass: '',
+      ItemTier: null,
+      CategoryCode: 40000,
+      SortCondition: 'ASC',
+    }),
+  });
+}
