0deac215d34f8dd42cc9935ade209770b89b27a8
jeongjiwon <wldnjs9705@gmail.com>
2026-01-19 17:47:03 +0900

    fix: padding 적용안되어 밀린 오류 수정

--- features/sidebar/components/setting-button.tsx
+++ features/sidebar/components/setting-button.tsx
@@ -15,13 +15,13 @@ export default function SettingButton() {
       <PopoverTrigger asChild>
         <Button
           variant="ghost"
-          className="hidden h-9 rounded-md text-sm font-medium px-2.5 py-2 xl:flex xl:w-full hover:bg-hover-background dark:hover:bg-hover-background group relative xl:justify-between xl:items-center duration-300"
+          className="hidden h-9 rounded-md text-sm font-medium !px-2.5 py-2 xl:flex xl:w-full hover:bg-hover-background dark:hover:bg-hover-background group relative xl:justify-between xl:items-center duration-300"
         >
           <Settings className="size-5 shrink-0" />
           <p
             className={cn(
               'absolute top-1/2 -translate-y-1/2 left-1/5 whitespace-nowrap transition-opacity opacity-0 text-base',
-              'group-data-[state-sidebar-open=true]:opacity-100 group-data-[state-sidebar-open=true]:duration-300 group-data-[state-sidebar-open=true]:delay-100'
+              'group-data-[state-sidebar-open=true]:opacity-100 group-data-[state-sidebar-open=true]:duration-300 group-data-[state-sidebar-open=true]:delay-100',
             )}
           >
             환경설정
