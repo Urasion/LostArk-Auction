7e63fe3e06d0fd7156052874458eec57972f2b85
jeongjiwon <wldnjs9705@gmail.com>
2026-01-20 16:57:52 +0900

    feat: 모바일 페이지에서 detail 경로로 가는 경우 hidden처리를 하기 위한 wrapper를 추가

---
+++ components/common/mobile-wrapper.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { ReactNode } from 'react';
+
+const wrapper_list = ['/gem', '/upgrade', '/recipe'];
+export default function MobileWrapper({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+
+  const isMainPage = wrapper_list.includes(pathname);
+  const mobileClass = isMainPage ? 'block' : 'hidden xl:block';
+
+  return (
+    <div
+      className={`
+        ${mobileClass} 
+        flex h-full shrink-0 
+        grow xl:grow-0 
+        justify-center xl:justify-start 
+        items-center xl:items-start
+      `}
+    >
+      {children}
+    </div>
+  );
+}
