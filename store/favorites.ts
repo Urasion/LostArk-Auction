056da14b6b69c0d000b5ebae7a43428bb2e65567
jeongjiwon <wldnjs9705@gmail.com>
2026-01-27 09:22:43 +0900

    feat: favorite interface 추가

---
+++ store/favorites.ts
@@ -0,0 +1,11 @@
+export interface FavoriteItem {
+  Id: number;
+  Name: string;
+  Grade: string;
+  Icon: string;
+  BasePrice: number;
+  CurrentPrice: number;
+  Stock: number;
+  ReturnAmount: number;
+  returnRate: number;
+}
