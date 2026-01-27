da61a861ced817bfc237cb415df9c830f4daa8ad
jeongjiwon <wldnjs9705@gmail.com>
2026-01-27 09:23:38 +0900

    feat: auction-item을 favorite-item형식으로 변경해 즐겨찾기를 등록할 때 사용하는 util을 추가

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,2 +1,18 @@
+import { AuctionItem } from '@/store/auction';
+import { FavoriteItem } from '@/store/favorites';
+
 export const delay = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export const convertAuctionItemToFavoriteItem = (
+  item: AuctionItem,
+): FavoriteItem => {
+  return {
+    Id: item.Id,
+    Name: item.Name,
+    Grade: item.Grade,
+    Icon: item.Icon,
+    BasePrice: item.CurrentMinPrice,
+    CurrentPrice: 0,
+  };
+};
