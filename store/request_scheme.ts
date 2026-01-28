f036ce3b11070ed9fea9901fa2048c719ca206e1
jeongjiwon <wldnjs9705@gmail.com>
2026-01-28 16:38:20 +0900

    fix: id와 current-price만 가져오게 변경하면서 scheme 변경

--- store/request_scheme.ts
+++ store/request_scheme.ts
@@ -1,13 +1,7 @@
 import z from 'zod';
 import { ROUTE_LIST } from './route';
-export const requestItemScheme = z.object({
+export const favoriteRequestItemScheme = z.object({
   Id: z.number(),
-  Name: z.string(),
-  Grade: z.string(),
-  Icon: z.string(),
-  BasePrice: z.number(),
-  CurrentPrice: z.number(),
-  Stock: z.number(),
   Type: z.enum(ROUTE_LIST),
 });
-export const requestScheme = z.array(requestItemScheme);
+export const favoriteRequestScheme = z.array(favoriteRequestItemScheme);
