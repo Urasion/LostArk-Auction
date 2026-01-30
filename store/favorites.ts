a06348e944667de19bbeef61cd3f4ddb15fd77c5
jeongjiwon <wldnjs9705@gmail.com>
2026-01-30 16:14:40 +0900

    feat: 입력시 runtime에서 잘못된 입력을 감지하기 위해서 zod 스키마 추가

--- store/favorites.ts
+++ store/favorites.ts
@@ -1,5 +1,5 @@
 import { ROUTE_LIST } from './route';
-
+import { z } from 'zod';
 export interface FavoriteItem {
   Id: number;
   Name: string;
@@ -15,3 +15,8 @@ export interface FavoriteItemResponse {
   Id: number;
   CurrentPrice: number;
 }
+
+export const priceScheme = z
+  .number()
+  .nonnegative({ message: '최소 0이상이어야 합니다.' })
+  .max(100_000_000, { message: '최대 1억까지 입력 가능합니다.' });
